8fecad470d1b7061c35cd9b1eaf06c6499cfb63b
Vanessa <vanessa-almeida@prodesan.com.br>
2022-10-07 14:52:24 -0300

    instalando telescope

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
 
         // provider criado para fazer o bind das interfaces de repository
         App\Providers\RepositoryServiceProvider::class,
