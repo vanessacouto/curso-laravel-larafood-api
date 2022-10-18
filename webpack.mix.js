cc803039d76631d805d83e2e8b3f3317dbe37dbd
Vanessa <vanessa-almeida@prodesan.com.br>
2022-10-18 15:51:35 -0300

    Atualizando Packages JS do Projeto

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .vue()
     .postCss('resources/css/custom-dash.css', 'public/css')
     .postCss('resources/css/site.css', 'public/css');
-    //.vue()
     //.sass('resources/sass/app.scss', 'public/css');
